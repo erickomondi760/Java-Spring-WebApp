20a38637b2385e920f90d2c97192119d0f588818
Erick <106428040+erickomondi760@users.noreply.github.com>
2026-01-13 07:02:10 +0300

    Add files via upload

---
+++ Database Script.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS `people`;
+USE `people`;
+
+--
+-- Table structure for table `people`
+--
+
+DROP TABLE IF EXISTS `occupations`;
+
+CREATE TABLE `occupations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+--
+-- Data for table `user`
+--
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `occupation_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_DETAIL_idx` (`occupation_id`),
+  CONSTRAINT `FK_DETAIL` FOREIGN KEY (`occupation_id`) REFERENCES `occupations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
